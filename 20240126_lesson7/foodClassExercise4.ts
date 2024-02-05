5bffeb83a5fb9889bdc5574241860bfa425f45de
Congying Zhao <t3cozh00@students.oamk.fi>
2024-02-05 19:58:00 +0200

    exercise4

--- 20240126_lesson7/foodClassExercise4.ts
+++ 20240126_lesson7/foodClassExercise4.ts
@@ -1,35 +1,37 @@
+/*
+   Exercise 4 (objects/exercises/4-classes.js): Create a class for Food which allows the user to set the type of food and quantity. The class should have a method, which can be used to eat the food and quantity will decrease accordingly and a method to be able to describe what type of food it is.
+*/
+
 class Food {
-    #name : string;
-    #remaining : number;
+  #name: string;
+  #remaining: number;
 
-    constructor(
-        name : string,
-        remaining : number){
-            this.#name = name;
-            this.#remaining = remaining;
-        }
-    whatIsThis(){
-        //console.log( this.#name );
+  constructor(name: string, remaining: number) {
+    this.#name = name;
+    this.#remaining = remaining;
+  }
+  whatIsThis() {
+    //console.log( this.#name );
+  }
+  eatOne() {
+    // or this.#remaining = this.#remaining -1
+
+    if (this.#remaining > 0) {
+      //this.#remaining -= 1; // ⚠️ 更新 #remaining的值
+      this.#remaining--;
+
+      console.log(
+        `Slurp! One ${this.#name.toLowerCase()} eaten. ${
+          this.#remaining
+        } remaining`
+      );
+    } else {
+      console.log(`Sorry, no more ${this.#name.toLowerCase()} remaining!`);
     }
-    eatOne(){
-
-         
-        // or this.#remaining = this.#remaining -1
-
-        if ( this.#remaining > 0){
-            //this.#remaining -= 1; // ⚠️ 更新 #remaining的值
-            this.#remaining --
-
-            console.log( `Slurp! One ${this.#name.toLowerCase()} eaten. ${this.#remaining} remaining`);
-            } else{
-            console.log( `Sorry, no more ${this.#name.toLowerCase()} remaining!`);
-
-        }
-    }
+  }
 }
 
-
-let banana = new Food('Banana', 4);
+let banana = new Food("Banana", 4);
 
 console.log(banana);
 
