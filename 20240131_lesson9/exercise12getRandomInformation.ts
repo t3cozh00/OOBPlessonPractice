7300dcc0ce4c2052a0e9168e0c817c0fb1176b77
Congying Zhao <t3cozh00@students.oamk.fi>
2024-02-05 20:03:22 +0200

    exercise12

--- 20240131_lesson9/exercise12getRandomInformation.ts
+++ 20240131_lesson9/exercise12getRandomInformation.ts
@@ -1,33 +1,39 @@
-const meteoritDataset = require('./y77d-th95.json');
+const meteoritDataset = require("./y77d-th95.json");
 
-//Create a function which returns a random meteroite. 
-//Use the function result to print out the meteorite information.
+/*
+  Exercise 12 
+  Use the dataset from exercise 8. Create a function which returns a random meteorite. 
+  Use the function result to print out the meteorite information (print out name, mass and year).
+*/
 
 interface Meteorite {
-name : string;
-id : string;
-nametype : string;
-recclass : string;
-mass : string;
-fall : string;
-year : string;
-reclat : string;
-reclong : string;
-geolocation : {
-   type : string;
-   coordinates : number [];
-};
-};
+  name: string;
+  id: string;
+  nametype: string;
+  recclass: string;
+  mass: string;
+  fall: string;
+  year: string;
+  reclat: string;
+  reclong: string;
+  geolocation: {
+    type: string;
+    coordinates: number[];
+  };
+}
 
+function getRandomInformation(data: Meteorite[]) {
+  const randomValue = Math.floor(Math.random() * data.length); //0.1 interg
 
-function getRandomInformation (data : Meteorite[]){
-
-    const randomValue = Math.floor(Math.random() * data.length);//0.1 interg
-
-    return data[randomValue];
-
-};
+  return data[randomValue];
+}
 
 const randomMeteorite = getRandomInformation(meteoritDataset);
 
-console.log(randomMeteorite.name + " " + randomMeteorite.mass + " " + randomMeteorite.year.slice(0, 4));
+console.log(
+  randomMeteorite.name +
+    " " +
+    randomMeteorite.mass +
+    " " +
+    randomMeteorite.year.slice(0, 4)
+);
