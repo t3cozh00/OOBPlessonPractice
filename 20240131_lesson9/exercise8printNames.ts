e4aaaf93edbc418b914be674a54e145eb0fa4aa6
Congying Zhao <t3cozh00@students.oamk.fi>
2024-02-05 20:06:17 +0200

    exercise8

--- 20240131_lesson9/exercise8printNames.ts
+++ 20240131_lesson9/exercise8printNames.ts
@@ -1,19 +1,24 @@
-const meteoritDataset = require('./y77d-th95.json'); // './' means from this folder the file could be found
-                                                     // '../' or '../../'
+/*
+  Exercise 8
+  Take his dataset of earth meteorite landings from NASA https://data.nasa.gov/resource/y77d-th95.json. 
+  Create an application which loads the dataset as a JSON file. 
+  You can load JSON file to node application with const someObject = require('./somefile.json'). 
+  Create a function, which prints the names of all meteorites.
+*/
 
- // Create a function, which prints the names of all meteorites.
-function printNames (data : any){
+const meteoritDataset = require("./y77d-th95.json"); // './' means from this folder the file could be found
+// '../' or '../../'
 
-    for ( let i = 0; i < data.length; i++ ){
-
-        console.log(data[i].name);
+// Create a function, which prints the names of all meteorites.
+function printNames(data: any) {
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].name);
 
     //let  names : string = data[i].name;
     //return names; 如果是return的话，loop一次就停止了，只return第一个名字
-
+  }
 }
-};
 
 //console.log(printNames(meteoritDataset));
 
-printNames(meteoritDataset);//return 所有名字
+printNames(meteoritDataset); //return 所有名字
