f75691c26c05cf2b7bf74b82c636cc90e65638b5
Congying Zhao <t3cozh00@students.oamk.fi>
2024-02-06 19:57:37 +0200

    dom&debug

---
+++ 20200206_lesson11/example.js
@@ -0,0 +1,17 @@
+console.log("Hello from example.js! ");
+
+// let a = [1, 2, 5, 7];
+// function sumArray(arr) {
+//   let sum = 0;
+//   for (i = 0; i <= arr.lenght; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+
+let a = document.getElementById("test");
+console.log(a);
+// recording 58:21
+
+// console.log("Text value of the element is");
+console.log(a.innerText);
